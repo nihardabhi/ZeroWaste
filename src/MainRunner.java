71cde52a9f3bb18a1bf92b8b20450018ce319f9d
nihardabhi <dabhi.nihar.5656@gmail.com>
2025-11-06 14:59:48 -0500

    Add JavaFX project structure for food waste management system

---
+++ src/MainRunner.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainRunner extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/dashboard.fxml"));
+        primaryStage.setTitle("ZeroWaste - Food Management");
+        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
